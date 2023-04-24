756d16d7f8d08a146953d70fe3c729d1a95fb9e5
dwkns <dwkns@me.com>
2023-04-24 19:25:38 +0100

    Add basic coupon model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  some_value = SecureRandom.hex(3)
+  Coupon.create(value: some_value, redeemed: false)
+end
